559542d13b593ea18c6107dac5243dc619f6eff1
pemtshewang <pemtshewang500@gmail.com>
2023-04-03 21:34:39 +0600

    added empty notes

--- frontend/routes/userNotes.jsx
+++ frontend/routes/userNotes.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import LogoutDialog from "../src/components/LogoutDialog";
+import { useQuery, QueryClientProvider } from "@tanstack/react-query";
 
 const UserNotePage = () => {
+  const queryClient = useQuery();
   const user = JSON.parse(localStorage.getItem("user"));
   const [isLogoutDialogOpen, setIsLogoutDialogOpen] = React.useState(false);
   return (
@@ -13,22 +15,25 @@ const UserNotePage = () => {
           setIsOpen={setIsLogoutDialogOpen}
         />
       )}
-      <nav className="flex pt-5 pl-5">
+      <nav className="flex pt-5 pl-5 h-5">
         <NavLink to="/">
           <div className="flex flex-col ">
             <div className="font-raleway text-2xl font-black">Many</div>
             <div className="font-raleway text-2xl font-black">Notes</div>
           </div>
         </NavLink>
-        <div className="ml-auto py-5 px-9">
-          <div className="flex flex-col">
-            <span className="hover:text-blue-500">{user.name}</span>
-            <div className="border-2 border-black flex flex-col px-4 py-3 mt-3 w-max invisible group-hover:visible">
-              <NavLink className="hover:underline hover:text-red-500 font-raleway">Edit My Profile</NavLink>
-              <NavLink className="hover:underline hover:text-red-500 font-raleway"
+        <div className="ml-auto py-5 px-9 profile-drop">
+          <span className="hover:text-blue-500">{user.name}</span>
+          <div className=" border-2 border-black flex flex-col px-4 py-3 mt-3 w-max">
+            <NavLink className="hover:underline hover:text-red-500 font-raleway">
+              Edit My Profile
+            </NavLink>
+            <NavLink
+              className="hover:underline hover:text-red-500 font-raleway"
               onClick={() => setIsLogoutDialogOpen(true)}
-              >Logout</NavLink>
-            </div>
+            >
+              Logout
+            </NavLink>
           </div>
         </div>
       </nav>
@@ -42,7 +47,9 @@ const UserNotePage = () => {
           </p>
         </div>
         <div className="container mx-auto w-max border-4 border-black p-3">
-          <Outlet />
+          <QueryClientProvider client={queryClient}>
+            <Outlet />
+          </QueryClientProvider>
         </div>
       </div>
     </main>
